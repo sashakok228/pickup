11a4aaa0e6e873792622be6032c5e023df6ca4e7
dvd-denis <pikuldenis9@gmail.com>
2023-03-27 20:27:26 +0600

    gtds

--- app/src/main/java/com/example/myapplication/GtdsActivity.java
+++ app/src/main/java/com/example/myapplication/GtdsActivity.java
@@ -6,21 +6,32 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class GtdsActivity extends AppCompatActivity {
-    TextView txtgt,txtgt2;
+    LinearLayout zg1,zg2;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gtds);
-        txtgt=findViewById(R.id.btn_gt1);
-        txtgt=findViewById(R.id.btn_gt2);
-        txtgt.setOnClickListener(new View.OnClickListener() {
+        zg1=findViewById(R.id.zg1);
+        zg2=findViewById(R.id.zg2);
+        zg1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent i = new Intent()
+                Intent i = new Intent();
+                i.putExtra("slovar",1);
+                startActivity(i);
+            }
+        });
+        zg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.putExtra("slovar",2);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/example/myapplication/SlovarDobav.java
+++ app/src/main/java/com/example/myapplication/SlovarDobav.java
@@ -2,13 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SlovarDobav extends AppCompatActivity {
-
+    TextView txt;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slovar_dobav);
+        txt=findViewById(R.id.nazvan);
+        Intent arg=getIntent();
+        int slov=arg.getIntExtra("slovar",0);
+        switch (slov){
+            case 1:
+                txt.setText("Начальные глаголы");
+                break;
+            case 2:
+                txt.setText("Начальные существительные");
+                break;
+        }
     }
 }
